147a92064623961fc464bd21f8450e7fc89d3683
timofruehwirth <py.codemonkey@proton.me>
2026-02-24 11:46:23 +0100

    feat(src): add JSON fetching to `generate_tei_templates.py` (#3)

--- src/generate_tei_templates.py
+++ src/generate_tei_templates.py
@@ -1,1 +1,27 @@
-print("hallo, let's create some nice templates from baserows metadata")
+# Import
+
+# Standard libraries
+import logging
+
+# External packages
+import requests
+from lxml import etree
+
+# Configure logging
+
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+# Constants
+
+JSON_URL = "https://raw.githubusercontent.com/krp-project/krp-baserow-dump/main/json_dumps/protocols.json"
+
+# Fetch JSON data
+
+def fetch_json():
+
+    with requests.get(JSON_URL, timeout=30) as response:
+        response.raise_for_status()
+        data = response.json()
+        logger.info("JSON successfully downloaded")
+        return data
